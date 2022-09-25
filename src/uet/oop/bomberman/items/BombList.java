c800be59224ca722fea165871d5e70d73072b873
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2022-09-25 11:57:22 +0700

    Add Music Explosion Sound

--- src/uet/oop/bomberman/items/BombList.java
+++ src/uet/oop/bomberman/items/BombList.java
@@ -1,13 +1,18 @@
 package uet.oop.bomberman.items;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import uet.oop.bomberman.entities.Bomber;
 import uet.oop.bomberman.graphics.Board;
 import uet.oop.bomberman.graphics.FlameSprite;
 import uet.oop.bomberman.graphics.Sprite;
 
+import java.io.File;
 import java.util.List;
 
+
 public class BombList extends Board {
+
     public BombList(int x, int y) {
         playGround =new Bomb[x][y];
     }
@@ -25,8 +30,12 @@ public class BombList extends Board {
                     Bomb b =(Bomb) playGround[i][j];
                     if (b.willExplode()) {
                         flameSpriteList.add(new FlameSprite(board, i ,j));
+                        Media media1 = new Media(new File("res/audio/Explosion.wav").toURI().toString());
+                        MediaPlayer mediaPlayer1 = new MediaPlayer(media1);
+                        mediaPlayer1.play();
                         remove(i, j);
                         bomberman.restoreABomb();
+
                     }
                 }
     }
